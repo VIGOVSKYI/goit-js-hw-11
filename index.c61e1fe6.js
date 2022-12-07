d2b8e464b46eb9b5686ad7f51a0ce610257ae353
VIGOVSKYI <107137747+VIGOVSKYI@users.noreply.github.com>
2022-12-07 18:16:56 +0000

    Deploying to gh-pages from @ VIGOVSKYI/goit-js-hw-11@3aee0a049475ced9d456d879effc590291a2df9d 🚀

---
+++ index.c61e1fe6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-form");document.querySelector(".gallery");e.addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements.searchQuery.value;console.log(t);var n="https://pixabay.com/api/?key=31894288-d396035e6b984cce02ff6ba47\n    &q=".concat(t,"&per_page=10&page=1\n    &image_type=fhoto&orientation=horizontal&safesearch=true");fetch(n).then((function(e){return e.json()})).then((function(e){return console.log(e)}))}))}();
+//# sourceMappingURL=index.c61e1fe6.js.map
