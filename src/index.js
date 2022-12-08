027a74fadb2877328d5a34b4671459232b5dfce5
VIGOVSKYI <vuv3000@ukr.net>
2022-12-08 07:52:52 +0200

    2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const galleryContainer = document.querySelector(".gallery");
 
 searchFormRef.addEventListener("submit",onSearch);
 
-function onSearch(e) {
+async function onSearch(e) {
     e.preventDefault();
 
     const searchValue = e.currentTarget.elements.searchQuery.value;
@@ -20,10 +20,18 @@ function onSearch(e) {
     const URL = `https://pixabay.com/api/?key=${API_KEY}
     &q=${searchValue}&per_page=10&page=1
     &image_type=fhoto&orientation=horizontal&safesearch=true`
-
-    axios(URL).then(data => console.log(data)).catch(error => console.log(error));
-
+  
+try {
+    const response = await axios.get(URL);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+  // axios(URL).then(data => console.log(data)).catch(error => console.log(error));
 };
+
+
+
 
     
 
