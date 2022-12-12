6bf137cf3ab63a940077d9e784ec0168fce2ea38
VIGOVSKYI <vuv3000@ukr.net>
2022-12-12 14:08:32 +0200

    1

---
+++ src/img-service.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export class ImgApiService {
+
+    constructor() { 
+        this.searchValue = "";
+        this.page = 1;
+    
+}
+
+async axiosApiImg() {
+    
+  console.log(this);
+
+  const API_KEY = "31953746-f18352ce1cc5baacf746e1b6b";
+
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchValue}&per_page=10&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
+  
+       return await axios.get(URL).then(response => {
+
+            console.log(response.data.hits);
+            this.page += 1;
+     
+            return response.data.hits;
+
+        });
+    };
+
+    resetPage() {
+        this.page = 1;
+    
+    }
+    
+    get query () {
+        return this.searchValue;
+    }
+
+    set query(NewQuery) {
+        this.searchValue = NewQuery;
+    }
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,94 +1,111 @@
 import './css/styles.css';
 
-import axios from "axios";
+import { ImgApiService } from "./img-service";
+
 
 const searchFormRef = document.querySelector("#search-form");
 
-const galleryContainer = document.querySelector(".gallery");
+const loadMoreBtnRef = document.querySelector(".load-more");
+
+// const galleryContainer = document.querySelector(".gallery");
+
+const  imgApiService  = new ImgApiService();
 
 searchFormRef.addEventListener("submit", onSearch);
 
+loadMoreBtnRef.addEventListener("click", loadMore);
 
-async function onSearch(e) {
+
+
+
+
+function onSearch(e) {
     e.preventDefault();
 
-    const searchValue = e.currentTarget.elements.searchQuery.value;
-
-    console.log(searchValue);
-
-    const API_KEY = "31894288-d396035e6b984cce02ff6ba47";
-
-    const URL = `https://pixabay.com/api/?key=${API_KEY}
-    &q=${searchValue}&per_page=10&page=1
-    &image_type=photo&orientation=horizontal&safesearch=true`
+  imgApiService.query = e.currentTarget.elements.searchQuery.value;
+  imgApiService.resetPage();
+  console.log(imgApiService.query);
+  imgApiService.axiosApiImg().then(renderImg);
   
- 
-  // const response =
-  await axios.get(URL).then(response => {response.map((key) => 
 
-      `<div class="photo-card">
-  <img src=${key.webformatURL} alt=${key.tags} loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${key.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${key.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${key.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${key.downloads}</b>
-    </p>
-  </div>
+
+    // axios(URL).then(data => console.log(data)).catch(error => console.log(error));
+};
+  
+
+function loadMore() {
+  imgApiService.axiosApiImg().then(renderImg);
+
+
+}
+
+function renderImg() {
+
+const markup = imgApiService.axiosApiImg().map((key) =>
+        `<div class="photo-card">
+<img src=${key.webformatURL} alt=${key.tags} loading="lazy" />
+<div class="info">
+  <p class="info-item">
+    <b>Likes ${key.likes}</b>
+  </p>
+  <p class="info-item">
+    <b>Views ${key.views}</b>
+  </p>
+  <p class="info-item">
+    <b>Comments ${key.comments}</b>
+  </p>
+  <p class="info-item">
+    <b>Downloads ${key.downloads}</b>
+  </p>
+</div>
 </div>`)
     .join("");
-
     console.log(markup);
-      
-    });
-  
-  console.log(response);
-  
-  // const objectPhoto = response.json();
-  
-  // console.log(objectPhoto);
-  
-  //   return objectPhoto;
+
+  galleryContainer.insertAdjacentHTML("beforeend", markup);
+}
   
 
-// axios(URL).then(data => console.log(data)).catch(error => console.log(error));
-};
 
 
 
 
 
-// const markup = Object.map((key) => 
 
-//       `<div class="photo-card">
-//   <img src=${key.webformatURL} alt=${key.tags} loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes ${key.likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${key.views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${key.comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${key.downloads}</b>
-//     </p>
-//   </div>
-// </div>`)
-//     .join("");
 
-//     console.log(markup);
 
-//   galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
 
 
     
