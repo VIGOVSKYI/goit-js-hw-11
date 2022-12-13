7195e1c68e3fb0c536ebef59de007222b6c6ba59
VIGOVSKYI <vuv3000@ukr.net>
2022-12-13 14:37:21 +0200

    3

--- src/img-service.js
+++ src/img-service.js
@@ -4,9 +4,10 @@ export class ImgApiService {
 
     constructor() { 
         this.searchValue = "";
+        
         this.page = 1;
+    }
     
-}
 
 async axiosApiImg() {
     
@@ -14,29 +15,30 @@ async axiosApiImg() {
 
   const API_KEY = "31953746-f18352ce1cc5baacf746e1b6b";
 
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchValue}&per_page=10&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchValue}&per_page=100&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
   
        return await axios.get(URL).then(response => {
+        // console.log("response.data.totalHits",response.data.totalHits);
 
-            console.log(response.data.hits);
+            // console.log(response.data.hits);
             this.page += 1;
      
-            return response.data.hits;
+            return response.data;
 
         });
     };
 
     resetPage() {
         this.page = 1;
+    };
     
-    }
     
     get query () {
         return this.searchValue;
-    }
+    };
 
     set query(NewQuery) {
         this.searchValue = NewQuery;
-    }
+    };
 
-}
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './css/styles.css';
 
 import { ImgApiService } from "./img-service";
 
+import Notiflix from 'notiflix';
+
 
 const searchFormRef = document.querySelector("#search-form");
 
@@ -15,27 +17,72 @@ searchFormRef.addEventListener("submit", onSearch);
 
 loadMoreBtnRef.addEventListener("click", loadMore);
 
+function showloadMoreBtn() {
+loadMoreBtnRef.classList.remove("is-hidden");
+ }
+
+function hideloadMoreBtn() {
+loadMoreBtnRef.classList.add("is-hidden");
+}
+
+function clearContainer() {
+  galleryContainer.innerHTML = "";
+}
+
+hideloadMoreBtn();
 
 function onSearch(e) {
+  e.preventDefault();
 
-    e.preventDefault();
-  // galleryContainer.innerHTML = "";
-  imgApiService.query = e.currentTarget.elements.searchQuery.value;
-  imgApiService.resetPage();
-  console.log(imgApiService.query);
-  imgApiService.axiosApiImg();
+  hideloadMoreBtn();
   
+  clearContainer();
+  
+  imgApiService.query = e.currentTarget.elements.searchQuery.value;
+
+  
+  imgApiService.resetPage();
+  
+  // console.log(imgApiService.query);
+  
+  imgApiService.axiosApiImg().then(renderImg);
+  
+  showloadMoreBtn();
 };
 
 function loadMore() {
   imgApiService.axiosApiImg().then(renderImg);
-}
+  }
 
 function renderImg() {
+  imgApiService.axiosApiImg().then(data => {
 
-const markup = imgApiService.axiosApiImg().map((key) =>
-        `<div class="photo-card">
-<img src=${key.webformatURL} alt=${key.tags} loading="lazy" />
+  console.log("data", data);
+    
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+    "Sorry, there are no images matching your search query. Please try again.",
+             {
+               fontSize: '15px',
+               timeout: 3000,
+             }
+               )
+    };
+    // if (data.totalHits) {
+    //   hideloadMoreBtn();
+    // Notiflix.Notify.failure(
+    // "We're sorry, but you've reached the end of search results.",
+    //          {
+    //            fontSize: '15px',
+    //            timeout: 3000,
+    //          }
+    //            )
+   
+    //          }
+  
+  const markup = data.hits.map((key) =>
+      `<div class="photo-card">
+<img src=${key.webformatURL} alt=${key.tags} loading="lazy" width="300" height="300"/>
 <div class="info">
   <p class="info-item">
     <b>Likes ${key.likes}</b>
@@ -51,11 +98,38 @@ const markup = imgApiService.axiosApiImg().map((key) =>
   </p>
 </div>
 </div>`)
-    .join("");
-    console.log(markup);
+      .join("");
+    // console.log(markup);
 
-  galleryContainer.insertAdjacentHTML("beforeend", markup);
-}
+    galleryContainer.insertAdjacentHTML("beforeend", markup);
+  })
+  .catch(error => {
+    // if (error) {
+    //   hideloadMoreBtn();
+
+    // Notiflix.Notify.failure(
+    // "We're sorry, but you've reached the end of search results.",
+    //          {
+    //            fontSize: '15px',
+    //            timeout: 3000,
+    //          }
+    //            )
+    //          }
+ 
+});
+};
+
+
+
+
+
+// Notiflix.Notify.success(
+//              "Too many matches found. Please enter a more specific name.",
+//              {
+//                fontSize: '15px',
+//                timeout: 1500,
+//              }
+//            )
   
 
 
